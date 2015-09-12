c4168a7035ae8d2b231c7d3927cb071d1bfe5f52
charlieHall <ch12953@my.bristol.ac.uk>
2015-09-12 18:43:32 +0100

    added home button

--- slowScroll.js
+++ slowScroll.js
@@ -3,16 +3,8 @@ $( document ).ready( function( $ ) {
         doc = document.documentElement;
     $(window).scroll(function () {
 
-    var backgroundPos = body.style.backgroundPosition.split(" ");
-	//now contains an array like ["0%", "50px"]
-	var xPos = backgroundPos[0],
-    yPos = backgroundPos[1];
-    console.log(backgroundPos);
-    var yInt = parseInt(yPos);
 
-    	if(typeof yInt !== "undefined") {
         	body.style.backgroundPosition = "0px " + (0-(Math.max(doc.scrollTop, body.scrollTop) / 50) ) + "px";
-        }
 
     });
 });
