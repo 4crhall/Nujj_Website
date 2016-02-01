d16a5dcf5ca043a5ae0c58c5b3dad31f925e1dc7
charlieHall <ch12953@my.bristol.ac.uk>
2016-02-01 17:03:09 +0000

    changed alexs name

---
+++ slowScroll.js
@@ -0,0 +1,10 @@
+$( document ).ready( function( $ ) {
+	var body = document.body,
+        doc = document.documentElement;
+    $(window).scroll(function () {
+
+
+        	body.style.backgroundPosition = "0px " + (0-(Math.max(doc.scrollTop, body.scrollTop) / 50) ) + "px";
+
+    });
+});
